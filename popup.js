ae18e649efcdede1550ead69f845dc52f53e2af1
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-09-26 00:33:44 +0100

    as copy editor can save, and send a preview to my client so they can see the new copy

--- popup.js
+++ popup.js
@@ -1,13 +1,23 @@
-// Initalize button with user's preferred color
 let editSite = document.getElementById("editSite");
+let savePreview = document.getElementById("savePreview");
 
-// When the button is clicked, set current website contentEditable to true
+// When the editSite button is clicked, set current website contentEditable to true
 editSite.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: setPageContentEditable,
+  });
+});
+
+// When the savePreview button is clicked, save the document and upload it somewhere for preview
+savePreview.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: savePagePreview,
   });
 });
 
@@ -18,3 +28,21 @@ function setPageContentEditable() {
   alert("You can now edit any text on this page.");
 }
 
+// Save Page preview and generate share url
+function savePagePreview() {
+  console.log(document.documentElement.innerHTML);
+  fetch("https://copyeditor.karmacomputing.co.uk/echo", {
+      method: 'post',
+      headers: {
+        "Content-type": "text/html; charset=UTF-8"
+      },
+      body: document.documentElement.innerHTML
+    })
+    .then(json)
+    .then(function (data) {
+      console.log('Request succeeded with JSON response', data);
+    })
+    .catch(function (error) {
+      console.log('Request failed', error);
+    });
+}
